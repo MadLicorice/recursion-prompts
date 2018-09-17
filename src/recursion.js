58753592faade0584dedad893dd24acee67d0177
MadLicorice <madlicorice101@gmail.com>
2018-09-16 20:08:24 -0700

    Refactor sum

--- src/recursion.js
+++ src/recursion.js
@@ -19,18 +19,10 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-<<<<<<< HEAD
-	if (array.length === 0) {
-		return 0;
-	}
-	return array[0] + sum(array.slice(1));
-
-=======
   if (array.length === 0) {
     return 0;
   }
   return array[0] + sum(array.slice(1));
->>>>>>> d075880392c9ee9f82fa1e9358865a523683898b
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
